4d4ba3feef4a7a1a15ef0b5582ef805dd4dc8ce5
b1nary <darkzxxz@gmail.com>
2017-01-04 10:53:13 -0500

    Update binary.py

--- main/binary.py
+++ main/binary.py
@@ -1,1 +1,9 @@
 print 'Hello World'
+#for python 2.7
+import sys
+test_cases = open(sys.argv[1], 'r')
+for test in test_cases:
+    test = test.replace('\n', '')
+    test = test.split(' ')
+    print(test[len(test)-2])
+print 'this is error
