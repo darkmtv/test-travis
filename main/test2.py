63c326b1a26c2260879ffffa465f7c381b80c470
darkmtv <suescun_22@hotmail.com>
2017-01-06 14:27:34 -0500

    Update test2.py

--- main/test2.py
+++ main/test2.py
@@ -4,9 +4,11 @@ with open(sys.argv[1], 'r') as test_cases:
     cont2 = 0
     for test in test_cases:
         stringe = test.strip()
-        newstring =""
-        dicci = {1:', yeah!',2:', this is crazy, I tell ya.',3:', can U believe this?',
-        4:', eh?',5:', aw yea.',6:', yo.',7:'? No way!',8:'. Awesome!'}
+        newstring = ""
+        dicci = {1: ', yeah!', 2: ', this is crazy, I tell ya.',
+                 3: ', can U believe this?',
+                 4: ', eh?', 5: ', aw yea.', 6: ', yo.',
+                 7: '? No way!', 8: '. Awesome!'}
         # .!.!
         # .
         for item in stringe:
@@ -14,12 +16,12 @@ with open(sys.argv[1], 'r') as test_cases:
                 cont = 1
             if (item == "." or item == "?" or item == "!") and cont2 == 1:
                 newstring += dicci[cont]
-                cont +=1
+                cont += 1
                 cont2 = 0
-            elif (item == "." or item == "?" or item == "!") and cont2== 0:
+            elif (item == "." or item == "?" or item == "!") and cont2 == 0:
                 newstring += item
                 cont2 = 1
             else:
                 newstring += item
-                
+
         print (newstring)
